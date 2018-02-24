6fc1bd6f99de62e617af03292e82c826c5486011
daniel glenn <linuxbender@gmail.com>
2018-02-24 15:23:51 +0100

    trying out to write some tests for react

--- react-fundamentals/contacts/src/ListContacts.spec.js
+++ react-fundamentals/contacts/src/ListContacts.spec.js
@@ -8,7 +8,48 @@ describe('<ListContacts/> component tests', () => {
         shallow(<ListContacts />);
     });
     it('Contains <ol/> element and has className contact-list', () => {
-        const wrapper = shallow(<ListContacts />);
-        expect(wrapper.find('ol')).toHaveClassName('contact-list');
+        const testee = shallow(<ListContacts />);
+        expect(testee.find('ol')).toHaveClassName('contact-list');
+    });
+
+    it('When contacts is empty then should no list items <li> displayed ', () => {
+        const testee = shallow(<ListContacts />);
+        expect(testee.find('li')).toBeEmpty();
+    });
+
+    it('When contacts is set with an empty list then should no list items <li> displayed ', () => {
+        const contacts = [];
+
+        const testee = shallow(<ListContacts contacts={contacts} />);
+
+        expect(testee.find('li')).toBeEmpty();
+    });
+
+    it('When contacts is set with one item then should one list item displayed', () => {
+        const contacts = [{
+            "id": "ryan",
+            "name": "Ryan Florence",
+            "email": "ryan@reacttraining.com",
+            "avatarURL": "http://localhost:5001/ryan.jpg"
+        }];
+
+        const testee = shallow(<ListContacts contacts={contacts} />);
+
+        expect(testee.find('li')).toHaveLength(1);
+    });
+    it('When contacts is set with one item then should props.contacts have one item', () => {
+
+        const contacts = [{
+            "id": "ryan",
+            "name": "Ryan Florence",
+            "email": "ryan@reacttraining.com",
+            "avatarURL": "http://localhost:5001/ryan.jpg"
+        }];
+
+        const testee = shallow(<ListContacts contacts={contacts} />);
+        expect(testee.props('contacts').children).toHaveLength(1);
+        expect(testee.contains(contacts[0].email)).toBeTruthy();
+        expect(testee.contains(contacts[0].name)).toBeTruthy();
     });
 });
+
