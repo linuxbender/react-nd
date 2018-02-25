966e8777e6cf1c326c362277003e14e3f849c252
daniel glenn <linuxbender@gmail.com>
2018-02-25 23:40:34 +0100

    add form-serialize and setup fromsubmit to the server

--- react-fundamentals/contacts/src/App.js
+++ react-fundamentals/contacts/src/App.js
@@ -22,13 +22,26 @@ class App extends Component {
     ContactsAPI.remove(contact);
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }))
+    })
+  }
+
   render() {
     return (
       <div>
         <Route exact path="/" render={() => (
           <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
         )} />
-    <Route path="/create" component={CreateContact} />
+        <Route path="/create" render={({ history }) => (
+          <CreateContact onCreateContact={contact => {
+            this.createContact(contact);
+            history.push('/');
+          }} />
+        )} />
       </div>
     )
   }
