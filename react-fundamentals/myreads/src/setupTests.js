b24ea108d82c90f4b9e4af8d74c8aa85bcb64c15
daniel glenn <linuxbender@gmail.com>
2018-02-26 01:35:33 +0100

    reformat file

--- react-fundamentals/myreads/src/setupTests.js
+++ react-fundamentals/myreads/src/setupTests.js
@@ -4,10 +4,10 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 const localStorageMock = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    clear: jest.fn(),
-    token:''
-  };
-  
-  global.localStorage = localStorageMock
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+  token: ''
+};
+
+global.localStorage = localStorageMock
