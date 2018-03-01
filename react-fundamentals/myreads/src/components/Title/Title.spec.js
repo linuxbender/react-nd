4db7ad8b4c5420e7f0876bedd68f310d6af17e67
daniel glenn <linuxbender@gmail.com>
2018-03-01 23:18:21 +0100

    create title component

--- react-fundamentals/myreads/src/components/Title/Title.spec.js
+++ react-fundamentals/myreads/src/components/Title/Title.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Label from "./Label";
+import Title from "./Title";
 import { shallow } from 'enzyme';
 
 it('Component is loaded without crashing', () => {
-    shallow(<Label />);
+    shallow(<Title />);
 });
