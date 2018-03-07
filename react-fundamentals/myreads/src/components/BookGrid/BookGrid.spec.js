e960bd93c060bcca7323292fec0c131149e1df0c
daniel glenn <linuxbender@gmail.com>
2018-03-07 23:19:04 +0100

    test: update tests

--- react-fundamentals/myreads/src/components/BookGrid/BookGrid.spec.js
+++ react-fundamentals/myreads/src/components/BookGrid/BookGrid.spec.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import BookGrid from "./BookGrid";
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+
+const books = [{id: 1, name: "John"}];
 
 it('Component is loaded without crashing', () => {
-    shallow(<BookGrid books={[]} />);
+    shallow(<BookGrid books={books} />);
+});
+
+it('When BoogGrid component receve 1 book then shoud display one li entry  ', () => {
+    const testee = mount(<BookGrid books={books} />);
+
+    expect(testee.props().books[0].id).toBe(1);
 });
