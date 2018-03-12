d245b377ca22c4a84044bc004be5a8403e150e7f
daniel glenn <linuxbender@gmail.com>
2018-03-12 14:19:13 +0100

    feat: add first actions

---
+++ react-and-redux/udacimeals/src/actions/index.js
@@ -0,0 +1,22 @@
+// action names
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
+
+// action creators
+
+export const addRecipe = ({day, recipe, meal}) => {
+    return {
+        type: ADD_RECIPE,
+        recipe,
+        day,
+        meal
+    }
+};
+
+export const removeFromCalendar = ({day, meal}) => {
+    return {
+        type: REMOVE_FROM_CALENDAR,
+        day,
+        meal
+    }
+};
