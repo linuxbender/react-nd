9f5c83a87ca6f0ccafb70c97e78f597866a7e76c
daniel glenn <linuxbender@gmail.com>
2018-03-12 23:35:17 +0100

    feat: add redux and connect to the app

--- react-and-redux/udacimeals/src/components/App.js
+++ react-and-redux/udacimeals/src/components/App.js
@@ -1,49 +1,32 @@
 import React, {Component} from 'react';
-import {addRecipe} from '../actions'
-import '../App.css';
+import {connect} from 'react-redux';
+import {addRecipe, removeFromCalendar} from "../actions";
 
 class App extends Component {
-    state = {
-        calendar: null
-    };
-
-    // subscribe to the store
-    componentDidMount() {
-        const {store} = this.props;
-
-        store.subscribe(() => {
-            this.setState(() => ({
-                calendar: store.getState()
-            }))
-        })
-    }
-
-    // dispatch action addRecipe
-    submitFood = () => {
-        this.props.store.dispatch(addRecipe({
-            day: 'monday',
-            meal: 'breakfast',
-            recipe: {
-                label: this.input.value
-            },
-        }));
-
-        this.input.value = '';
-    };
-
     render() {
         return (
-            <div>
-                <input
-                    type='text'
-                    ref={(input) => this.input = input}
-                    placeholder="Monday's Breakfast"
-                />
-                <button onClick={this.submitFood}>Submit</button>
-                <pre>Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}</pre>
-            </div>
+            <div>Hello World</div>
         );
     }
 }
 
-export default App;
+const mapStateToProps = (calendar) => {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    return {
+        calendar: dayOrder.map(day => {
+            return {
+                day,
+                meals: Object.assign({}, calendar[day])
+            };
+        })
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        selectRecipe: (data) => dispatch(addRecipe(data)),
+        remove: (data) => dispatch(removeFromCalendar(data))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
