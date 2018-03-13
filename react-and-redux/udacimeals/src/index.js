1ba08003bf2b40e8106dae02ea57b2b43c667b9c
daniel glenn <linuxbender@gmail.com>
2018-03-13 20:21:46 +0100

    chore: cleanup and setup helper, api, css from the repo

--- react-and-redux/udacimeals/src/index.js
+++ react-and-redux/udacimeals/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import createStore from "redux/es/createStore";
 import reducer from './reducers';
 import {Provider} from 'react-redux';
@@ -11,4 +10,3 @@ const store = createStore(reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
-registerServiceWorker();
