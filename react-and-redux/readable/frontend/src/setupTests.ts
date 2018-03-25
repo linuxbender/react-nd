57eecc2d479240bc66e6240b16eff08e2857ed1d
daniel glenn <linuxbender@gmail.com>
2018-03-25 23:57:29 +0200

    feat: css header grid flex setup

--- react-and-redux/readable/frontend/src/setupTests.ts
+++ react-and-redux/readable/frontend/src/setupTests.ts
@@ -2,9 +2,9 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 export interface ILocalStorage {
-    getItem: ()=> void;
-    setItem: ()=> void;
-    clear: ()=> void;
+    getItem: () => void;
+    setItem: () => void;
+    clear: () => void;
     token: string;
 }
 const localStorageMock: ILocalStorage = {
