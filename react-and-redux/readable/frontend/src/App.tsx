f24d296f96f3419db206bd4f17e2d7584df33fa7
daniel glenn <linuxbender@gmail.com>
2018-04-29 18:02:22 +0200

    core: update version

--- react-and-redux/readable/frontend/src/App.tsx
+++ react-and-redux/readable/frontend/src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
                 </header>
                 <Title text="Mein neuer Title.." />
                 <p className="App-intro">To get started, edit <code> src / App.tsx</code> and save to reload.</p>
-                <Button />
+                <Button label="Demo 42"/>
                 <Button label="Hello Wolrd" disabled={false} />
             </div>
         );
