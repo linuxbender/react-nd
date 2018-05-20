3122ae4ab78d0f08c934c8b8455916ee2110b53a
daniel glenn <linuxbender@gmail.com>
2018-05-20 13:08:14 +0200

    refactor: rename reducer index file to rootReducer

--- react-and-redux/readable/frontend/src/store/appStore.js
+++ react-and-redux/readable/frontend/src/store/appStore.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
-import rootReducer from '../reducers';
+import rootReducer from '../reducers/rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
