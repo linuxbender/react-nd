66ac1e275dc37ad2e605f745edddae6d32f86430
daniel glenn <linuxbender@gmail.com>
2018-05-22 02:38:46 +0200

    fix: undefined in DetailManager

--- react-and-redux/readable/frontend/src/components/detail/DetailManager.js
+++ react-and-redux/readable/frontend/src/components/detail/DetailManager.js
@@ -11,14 +11,15 @@ import BreadCrumBar from './BreadCrumBar';
 import NewComment from './NewComment';
 import SortBar from './SortBar';
 
-// import DetailPost from './DetailPost';
-
 class DetailManager extends React.Component {
 
     constructor(props) {
         super(props);
         if (props.match.params.id && props.match.params.id === '') {
-            props.history.goBack();
+            props.history.push('/');
+        }
+        if (props.posts.length === 0) {
+            props.history.push('/');
         }
     }
 
@@ -32,8 +33,6 @@ class DetailManager extends React.Component {
         return (
             <div>
                 {this.props.isLoading && <Loader/>}
-                {!this.props.isLoading && this.props.post !== undefined && this.props.history.push('/')}
-                {!this.props.isLoading && this.props.post !== undefined && this.props.history.push('/404')}
                 {!this.props.isLoading && <BreadCrumBar/>}
                 {!this.props.isLoading && this.props.posts.map(post => <ArticlePost key={post.id} post={post}/>)}
                 {!this.props.isLoading && <SortBar/>}
@@ -45,13 +44,18 @@ class DetailManager extends React.Component {
     }
 }
 
-// {!this.props.isLoading && <DetailPost detail={this.state.detail}/>}
-
-const mapStateToProps = (state, ctx) => ({
-    isLoading: state.apiCallsInProgress > 0,
-    comments: state.comment,
-    posts: [...state.post.filter(i => i.id === ctx.match.params.id)],
-    postId: [...state.post.filter(i => i.id === ctx.match.params.id)][0].id
-});
+const mapStateToProps = (state, ctx) => {
+    let posts = [...state.post.filter(i => i.id === ctx.match.params.id)] || [];
+    let postId = '';
+    if (posts.length !== 0) {
+        postId = posts[0].id
+    }
+    return {
+        isLoading: state.apiCallsInProgress > 0,
+        comments: state.comment,
+        posts: posts,
+        postId: postId
+    };
+};
 
 export default withRouter(connect(mapStateToProps)(DetailManager));
