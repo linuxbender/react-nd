aa759659b32dc0cf709258b3eb375e1b62780573
daniel glenn <linuxbender@gmail.com>
2018-05-24 03:40:25 +0200

    feat: impl. feedback list order by datetime

--- react-and-redux/readable/frontend/src/reducers/commentReducer.js
+++ react-and-redux/readable/frontend/src/reducers/commentReducer.js
@@ -4,21 +4,21 @@ import {T_Store} from '../utils/typeHelper';
 const commentReducer = (state = T_Store.comment, action) => {
     switch (action.type) {
         case T.LOAD_COMMENTS_SUCCESS:
-            return action.data;
+            return action.data.sort((a, b) => b.timestamp - a.timestamp);
         case T.DELETE_COMMENT_SUCCESS:
-            return state.filter(i => i.id !== action.data.id);
+            return state.filter(i => i.id !== action.data.id).sort((a, b) => b.timestamp - a.timestamp);
         case T.CREATE_COMMENT_SUCCESS:
-            return [...state, action.data];
+            return [...state, action.data].sort((a, b) => b.timestamp - a.timestamp);
         case T.UPDATE_COMMENT_SUCCESS:
-            return [...state.filter(i => i.id !== action.data.id), action.data];
+            return [...state.filter(i => i.id !== action.data.id), action.data].sort((a, b) => b.timestamp - a.timestamp);
         case T.UPDATE_COMMENT_UP_VOTE_SUCCESS:
-            return [...state.filter(i => i.id !== action.data.id), action.data];
+            return [...state.filter(i => i.id !== action.data.id), action.data].sort((a, b) => b.timestamp - a.timestamp);
         case T.UPDATE_COMMENT_DOWN_VOTE_SUCCESS:
-            return [...state.filter(i => i.id !== action.data.id), action.data];
+            return [...state.filter(i => i.id !== action.data.id), action.data].sort((a, b) => b.timestamp - a.timestamp);
         case T.SORT_COMMENTS_BY_BEST_SCORE:
-            return [...state.filter(i => i).sort((a, b) => b.voteScore - a.voteScore)];
+            return [...state.filter(i => i).sort((a, b) => b.voteScore - a.voteScore)].sort((a, b) => b.timestamp - a.timestamp);
         case T.SORT_COMMENTS_BY_LOWEST_SCORE:
-            return [...state.filter(i => i).sort((a, b) => a.voteScore - b.voteScore)];
+            return [...state.filter(i => i).sort((a, b) => a.voteScore - b.voteScore)].sort((a, b) => b.timestamp - a.timestamp);
         default:
             return state;
     }
--- react-and-redux/readable/frontend/src/reducers/postReducer.js
+++ react-and-redux/readable/frontend/src/reducers/postReducer.js
@@ -4,19 +4,19 @@ import {T_Store} from '../utils/typeHelper';
 const postReducer = (state = T_Store.post, action) => {
     switch (action.type) {
         case T.LOAD_POSTS_SUCCESS:
-            return action.data.filter(i => i.deletet !== true);
+            return action.data.filter(i => i.deletet !== true).sort((a, b) => b.timestamp - a.timestamp);
         case T.LOAD_POSTS_BY_CATEGORY_SUCCESS:
-            return action.data;
+            return action.data.filter(i => i.deletet !== true).sort((a, b) => b.timestamp - a.timestamp);
         case T.CREATE_POST_SUCCESS:
-            return [...state, action.data];
+            return [...state, action.data].sort((a, b) => b.timestamp - a.timestamp);
         case T.UPDATE_POST_SUCCESS:
-            return [...state.filter(i => i.id !== action.data.id), action.data];
+            return [...state.filter(i => i.id !== action.data.id), action.data].sort((a, b) => b.timestamp - a.timestamp);
         case T.DELETE_POST_SUCCESS:
-            return state.filter(i => i.id !== action.data.id);
+            return state.filter(i => i.id !== action.data.id).sort((a, b) => b.timestamp - a.timestamp);
         case T.UPDATE_POST_UP_VOTE_SUCCESS:
-            return [...state.filter(i => i.id !== action.data.id), action.data];
+            return [...state.filter(i => i.id !== action.data.id), action.data].sort((a, b) => b.timestamp - a.timestamp);
         case T.UPDATE_POST_DOWN_VOTE_SUCCESS:
-            return [...state.filter(i => i.id !== action.data.id), action.data];
+            return [...state.filter(i => i.id !== action.data.id), action.data].sort((a, b) => b.timestamp - a.timestamp);
         case T.SORT_POSTS_BY_BEST_SCORE:
             return [...state.filter(i => i).sort((a, b) => b.voteScore - a.voteScore)];
         case T.SORT_POST_BY_LOWEST_SCORE:
