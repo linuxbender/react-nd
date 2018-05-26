4994cf8f40f6da54e64ad58c2e0ff173f7234e60
daniel glenn <linuxbender@gmail.com>
2018-05-26 09:47:08 +0200

    feat: add Live native tracking

--- react-native/udaciFitness/App.js
+++ react-native/udaciFitness/App.js
@@ -1,15 +1,18 @@
 import {FontAwesome, Ionicons} from '@expo/vector-icons'
 import {Constants} from 'expo'
 import React from 'react'
-import {Platform, StatusBar, StyleSheet, View} from 'react-native'
-import {createMaterialTopTabNavigator, createStackNavigator} from 'react-navigation'
+import {Platform, StatusBar, View} from 'react-native'
+import {createStackNavigator, createMaterialTopTabNavigator} from 'react-navigation'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import AddEntry from './components/AddEntry'
 import EntryDetail from './components/EntryDetail'
 import History from './components/History'
+import Live from './components/Live'
 import reducer from './reducers'
 import {purple, white} from './utils/colors'
+import {setLocalNotification} from './utils/helpers'
+
 
 function UdaciStatusBar({backgroundColor, ...props}) {
     return (
@@ -20,39 +23,47 @@ function UdaciStatusBar({backgroundColor, ...props}) {
 }
 
 const Tabs = createMaterialTopTabNavigator({
-    History: {
-        screen: History,
-        navigationOptions: {
-            tabBarLabel: 'History',
-            tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+        History: {
+            screen: History,
+            navigationOptions: {
+                tabBarLabel: 'History',
+                tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+            }
+        },
+        AddEntry: {
+            screen: AddEntry,
+            navigationOptions: {
+                tabBarLabel: 'Add Entry',
+                tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+            }
+        },
+        Live: {
+            screen: Live,
+            navigationOptions: {
+                tabBarLabel: 'Live',
+                tabBarIcon: ({tintColor}) => <Ionicons name='ios-speedometer' size={30} color={tintColor}/>
+            }
         },
     },
-    AddEntry: {
-        screen: AddEntry,
+    {
         navigationOptions: {
-            tabBarLabel: 'Add Entry',
-            tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+            header: null
         },
-    },
-}, {
-    navigationOptions: {
-        header: null
-    },
-    tabBarOptions: {
-        activeTintColor: Platform.OS === 'ios' ? purple : white,
-        style: {
-            height: 56,
-            backgroundColor: Platform.OS === 'ios' ? white : purple,
-            shadowColor: 'rgba(0, 0, 0, 0.24)',
-            shadowOffset: {
-                width: 0,
-                height: 3
-            },
-            shadowRadius: 6,
-            shadowOpacity: 1
+        tabBarOptions: {
+            activeTintColor: Platform.OS === 'ios' ? purple : white,
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === 'ios' ? white : purple,
+                shadowColor: 'rgba(0, 0, 0, 0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+            }
         }
-    }
-});
+    });
 
 const MainNavigator = createStackNavigator({
     Home: {
@@ -74,16 +85,12 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <Provider store={this.store}>
-                <View style={styles.container}>
-                    <UdaciStatusBar backgroundColor={purple} barStyle="light-content"/>
+            <Provider store={createStore(reducer)}>
+                <View style={{flex: 1}}>
+                    <UdaciStatusBar backgroundColor={purple} barStyle='light-content'/>
                     <MainNavigator/>
                 </View>
             </Provider>
-        );
+        )
     }
 }
-
-const styles = StyleSheet.create({
-    container: {flex: 1}
-});
