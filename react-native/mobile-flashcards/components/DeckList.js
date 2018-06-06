cabfd9ba61fd668da119080ffaac9c3223adae93
daniel glenn <linuxbender@gmail.com>
2018-06-06 19:57:30 +0200

    core: clean up

--- react-native/mobile-flashcards/components/DeckList.js
+++ react-native/mobile-flashcards/components/DeckList.js
@@ -13,9 +13,6 @@ class DeckList extends Component {
 
     componentDidMount() {
         this.props.onLoadDecks();
-        /*
-            clearLocalNotification)
-            setLocalNotification)*/
     }
 
     render() {
--- react-native/mobile-flashcards/utils/constants.js
+++ react-native/mobile-flashcards/utils/constants.js
@@ -3,20 +3,16 @@ import {StyleSheet} from 'react-native'
 export const STORAGE_KEY = 'com.theforce:mobile-flashcards:storage';
 export const NOTIFICATION_KEY = 'com.theforce:mobile-flashcards:notification';
 
-// https://coolors.co/083d77-ebebd3-f4d35e-ee964b-f95738
-
 export const lightBlue = '#1d4992';
 export const darkBlue = '#083d77';
 export const deepGreen = '#01641d';
 export const eggShell = '#ebebd3';
 export const yellow = '#f4d35e';
 export const orange = '#f95738';
-export const softRed = '#e0413b';
 export const pink = '#ff084a';
 
 // other colors
 export const white = '#ffffff';
-export const blackLicorice = '#212121';
 export const black = '#000000';
 
 // styles
