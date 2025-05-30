d882ed307e138e02af44876fe3882f5de775a70f
parzival nieminen <parzival.nieminen@gmail.com>
2025-05-31 00:27:23 +0200

    REFACTOR: update item text in App component for consistency

--- react-kata-03/src/App.tsx
+++ react-kata-03/src/App.tsx
@@ -4,9 +4,9 @@ import List, {type Info, type ListProps} from "./components/list/List.tsx";
 const App = () => {
 
     const data: Info[] = [
-        {id: 1, text: 'Item 1', btnText: 'Button 1'},
-        {id: 2, text: 'Item 2', btnText: 'Button 2'},
-        {id: 3, text: 'Item 3', btnText: 'Button 3'}
+        {id: 1, text: 'List Item 1', btnText: 'Button 1'},
+        {id: 2, text: 'List Item 2', btnText: 'Button 2'},
+        {id: 3, text: 'List Item 3', btnText: 'Button 3'}
     ];
 
     const listProps: ListProps<Info> = {data};
