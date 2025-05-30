9e6ad6f1dbbaf49af5fcbdbc44d81635c564f9b6
parzival nieminen <parzival.nieminen@gmail.com>
2025-05-31 00:19:27 +0200

    TEST: add missing line for improved readability in App test

--- react-kata-03/src/App.test.tsx
+++ react-kata-03/src/App.test.tsx
@@ -4,6 +4,7 @@ import {render, screen} from '@testing-library/react'
 import App from './App'
 
 describe('TEST: App', () => {
+
     it('zeigt den Titel "Vite + React" an', () => {
         render(<App/>)
         expect(screen.getByText('Vite + React')).toBeInTheDocument()
