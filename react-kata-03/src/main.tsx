3277e6a98a1bad408da0df665197af9bc61053e1
parzival nieminen <parzival.nieminen@gmail.com>
2025-06-01 23:49:48 +0200

    REFACTOR: replace BrowserRouter with RouterProvider and create routes for improved routing structure

--- react-kata-03/src/main.tsx
+++ react-kata-03/src/main.tsx
@@ -1,13 +1,18 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './assets/index.css'
-import App from './App.tsx'
-import {BrowserRouter} from "react-router";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import App from "./App.tsx";
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+    }
+])
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <RouterProvider router={routes}/>
     </StrictMode>,
 )
