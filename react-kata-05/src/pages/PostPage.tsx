acf5d4021c5e730816ed9ec204e2f33ce39fe94b
linuxbender <linuxbender@gmail.com>
2025-06-23 00:46:54 +0200

    FEAT: add PostPage component for displaying individual posts

---
+++ react-kata-05/src/pages/PostPage.tsx
@@ -0,0 +1,9 @@
+export const PostPage = () => {
+    return (
+        <div>
+            <h1>Post Page</h1>
+            <p>Welcome to the Post Page!</p>
+            <p>This is a simple React application demonstrating a post page.</p>
+        </div>
+    );
+}
